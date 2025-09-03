ab32c0f529a6072bbb1fa707a6d584411d7e0b4a
Srinjoy Sur <91840579+SrinjoySur@users.noreply.github.com>
2025-09-03 12:09:07 +0530

    Create tests/pageObjects/LoginPage.js

---
+++ tests/pageObjects/LoginPage.js
@@ -0,0 +1,20 @@
+class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.emailInput = page.locator('#email');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#login');
+  }
+
+  async navigate() {
+    await this.page.goto('https://example.com/login');
+  }
+
+  async login(email, password) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
+
+module.exports = LoginPage;
